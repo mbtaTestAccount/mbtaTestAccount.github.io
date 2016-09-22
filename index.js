4f9ccdaecd9cdee4172ab9e49dd21ddab39cb581
Alexander Burke <alexander.j.burke@gmail.com>
2016-09-21 22:12:27 -0400

    removed unused functions, splitting file on newline

--- index.js
+++ index.js
@@ -6,34 +6,12 @@ trainTimes.ProcessTrainData = function() {
 
   	var reader = new FileReader();
   	reader.onload = function(event) {
-    // The file's text will be printed here
-    console.log(event.target.result)
+  		var data = event.target.result.split('\r\n');
+  		alert(data[0]);
+  		alert(data[1]);
+  		alert(data[2]);
+  		alert(data.length);
   };
 
   reader.readAsText(file);
-
-
-
-	// alert(fileInput.name);
-	// alert(fileInput.size);
-	// alert(fileInput.type);
-	// if(fileInput){
-	// 	var FileReader = new FileReader();
-	// 	FileReader.onload = function(loadTarget){
-	// 		var content = loadTarget.target.result;
-	// 		alert(content.substr(1,10));
-	// 	}
-	// }
 };
-
-trainTimes.onChange = function(event) {
-  var file = event.target.files[0];
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    // The file's text will be printed here
-    console.log(event.target.result)
-  };
-
-  reader.readAsText(file);
-}
-
