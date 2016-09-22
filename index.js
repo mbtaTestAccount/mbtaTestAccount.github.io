b6f724eee0ba85a90ce42f37c8fe4830e278e622
Alexander Burke <alexander.j.burke@gmail.com>
2016-09-21 21:50:59 -0400

    added button to upload and method that should have read the file, but apparently something about FileReader has changed since I last used it. Time to debug

---
+++ index.js
@@ -0,0 +1,15 @@
+trainTimes = {};
+
+trainTimes.ProcessTrainData = function() {
+	var fileInput = document.getElementById('trainData').files[0];
+	
+	// if(fileInput){
+	// 	var FileReader = new FileReader();
+	// 	FileReader.onload = function(loadTarget){
+	// 		var content = loadTarget.target.result;
+	// 		alert(content.substr(1,10));
+	// 	}
+	// }
+};
+
+
