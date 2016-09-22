30cd1d2d53fda35cc45f96501d5a0998fa05e9bd
Alexander Burke <alexander.j.burke@gmail.com>
2016-09-21 22:33:27 -0400

    Getting train names, something wrong with converting epoch time still

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ trainTimes = {};
 trainTimes.ProcessTrainData = function() {
 
 	var file = document.getElementById('trainData').files[0];
-
+	var times = [];
   	var reader = new FileReader();
   	reader.onload = function(event) {
   		var data = event.target.result.split('\r\n');
-  		alert(data[0]);
-  		alert(data[1]);
-  		alert(data[2]);
-  		alert(data.length);
-  };
+  		var date = new Date(0);
+
+  		//-2 because the last line appears to be empty
+  		for(i = 1; i <= data.length-2; i++){
+  			var dataPoints = data[i].split(',');
+  			var localdate = date.setUTCSeconds(dataPoints[4]);
+  			times.push({
+  				TrainName : dataPoints[3],
+  				// DepartureTime :  localdate.setSeconds(localdate.getSeconds() + dataPoints[5])
+  			});
+  		};
+  		console.log(times);
+  	};
 
   reader.readAsText(file);
 };
